592f77fe0eb8529017a05725e99c4a9b97643297
Nishit <nishitranjan41@gmail.com>
2022-07-09 23:52:58 +0530

    this is about starting position of player

--- src/com/bridgelabz/SnakeandLadder.java
+++ src/com/bridgelabz/SnakeandLadder.java
@@ -4,14 +4,15 @@ public class SnakeandLadder
     public static void main(String[] args)
     {
         System.out.println(" welcome to snake ladder ");
-   //int player[]=new int[2];
-   //player[0]=0;// first player starting position
-    //player[1]=0;// second player starting position
-       // System.out.println(player[0]);
-        //System.out.println(player[1]);
+   int player[]=new int[2];
+   player[0]=0;// first player starting position
+    player[1]=0;// second player starting position
+        System.out.println("sarting position of player1"+player[0]);
+        System.out.println("Starting position of player2"+player[1]);
+        System.out.println(player[1]);
         //int position=0;
-      //  System.out.println("Game Begins");
-       // System.out.println("position 4,14,18,58 are ladders");
+        //System.out.println("Game Begins");
+        //System.out.println("position 4,14,18,58 are ladders");
         //System.out.println("position 10,25,38,48 99 are snakes");
         //player[position]+=(int)(Math.random()*10)%6+1;
         //System.out.println(player[position]);
